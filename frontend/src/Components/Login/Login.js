03f2ab69a31caac2f6043294372ed8c0d02dae4d
Ritika Saxena <ritikasaxena911@gmail.com>
2023-04-22 14:51:37 +0530

    added fetch

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import googleimg from '../../assets/google.png'
 import logo from '../../assets/logo.png'
 export default function Login() {
+
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const Navigate=useNavigate();
+  const[message,setMessage]=useState("");
+
+  const LoginData=()=>{
+    fetch("http://localhost:5000/login",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        email:email,
+        password:password
+      })
+    }).then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+      if(data.error){
+        setMessage(data.error);
+       console.log(data.error);
+      }
+      else{
+        setMessage(data.message);
+        console.log(data)
+        localStorage.setItem("jwt",data.token)
+        localStorage.setItem("user",JSON.stringify(data.user))
+        Navigate('/');
+       console.log(data.message);
+  }
+    })
+    .catch(err=>console.log(err))
+  }
+
   return (
     <div className='Login'>
           <img src={logo} alt=""  className='logo'/>
@@ -14,13 +49,16 @@ export default function Login() {
                 <img src={googleimg} alt="" />
                     Sign in with Google </button>
             </div>
+            <hr />
+        <span className='Or'>or</span>
+            <p className='message'>{message}</p>
         <div>
-           <input type="email" name='email' id='email' placeholder='Enter email' />
+           <input type="email" name='email' id='email' placeholder='Enter email' onChange={(e)=>{setEmail(e.target.value)}}/>
          </div>
          <div>
-           <input type="password" name='password' id='password' placeholder='Enter Password' />
+           <input type="password" name='password' id='password' placeholder='Enter Password' onChange={(e)=>{setPassword(e.target.value)}}/>
          </div>
-         <button className='LoginBtn' >Login</button>
+         <button className='LoginBtn' onClick={LoginData}>Login</button>
            <p className='noacct'>Don't have an account? <Link to='/signup'><span>SignUp</span></Link></p>
          </div>
     </div>
--- frontend/src/Components/Sign Up/SignUp.js
+++ frontend/src/Components/Sign Up/SignUp.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import googleimg from '../../assets/google.png'
 import logo from '../../assets/logo.png'
 
 export default function SignUp() {
+
+  const[userName,setUserName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[message,setMessage]=useState("");
+  const Navigate=useNavigate();
+  const postData=()=>{
+    fetch("http://localhost:5000/signup",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        userName:userName,
+        email:email,
+        password:password
+      })
+    }).then(res=>res.json())
+    .then(data=>{
+      if(data.error){
+        setMessage(data.error);
+        console.log(data.error);
+      }
+      else{
+        console.log(data.message);
+        setMessage(data.message);
+        Navigate('/login');
+      }
+    })
+    .catch(err=>console.log(err))
+  }
+
+
   return (
     <div className='SignUp'>
           <img src={logo} alt=""  className='logo'/>
@@ -14,16 +47,20 @@ export default function SignUp() {
                 <img src={googleimg} alt="" />
                     Sign up with Google </button>
             </div>
+            <hr />
+        <span className='Or'>or</span>
+
+       <p className='signup'>{message}</p>
             <div>
-           <input type="name" name='name' id='name' placeholder='Enter username' />
+           <input type="name" name='name' id='name' placeholder='Enter username' onChange={(e)=>{setUserName(e.target.value)}} />
          </div>
         <div>
-           <input type="email" name='email' id='email' placeholder='Enter email' />
+           <input type="email" name='email' id='email' placeholder='Enter email' onChange={(e)=>{setEmail(e.target.value)}}/>
          </div>
          <div>
-           <input type="password" name='password' id='password' placeholder='Enter Password' />
+           <input type="password" name='password' id='password' placeholder='Enter Password' onChange={(e)=>{setPassword(e.target.value)}}/>
          </div>
-         <button className='SignUpBtn' >Sign Up</button>
+         <button className='SignUpBtn' onClick={postData} >Sign Up</button>
            <p className='acct'>Already have an account? <Link to='/login'><span>Login</span></Link></p>
          </div>
     </div>
