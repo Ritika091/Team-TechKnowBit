ee17fd7062de5e5948519c11e5aa270ced9f4993
Ritika Saxena <ritikasaxena911@gmail.com>
2023-04-22 18:47:38 +0530

    added nav

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
+import UpperNav from '../UpperNav/UpperNav'
+import './Home.css'
 
 export default function Home() {
   return (
     <div className='Home'>
         <Sidebar/>
+        <UpperNav/>
     </div>
   )
 }
---
+++ frontend/src/Components/UpperNav/UpperNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './UpperNav.css'
+import SearchIcon from '@mui/icons-material/Search';
+import Avatar from '@mui/material/Avatar';
+
+export default function UpperNav() {
+  return (
+    <div className='UpperNav'>
+       <div className="Search">
+       <SearchIcon className='searchIcon' />
+       <input type="text" placeholder="What do you want to listen?" name="search"></input>
+       </div>
+       <div className="profile">
+       <Avatar alt="Remy Sharp" src=""  className='Ava'/>
+       <h3>Ritika</h3>
+       </div>
+    </div>
+  )
+}
