7e9f2aad366729bd2bbb52476fceda93e89a53cb
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-04-22 20:07:25 +0530

    added progress bar

--- frontend/src/Components/Player/Player.js
+++ frontend/src/Components/Player/Player.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './Player.css'
 import thumbnail from '../../assets/thumbnail.jpg'
-
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipPreviousRoundedIcon from '@mui/icons-material/SkipPreviousRounded';
+import SkipNextRoundedIcon from '@mui/icons-material/SkipNextRounded';
 
 export default function Player() {
   return (
@@ -12,7 +14,14 @@ export default function Player() {
             <span class="track-title">Song Title</span>
             <span class="artist-name">Artist Name</span>
             </div>
+
         </div>
+        <div className="resume">
+            <SkipPreviousRoundedIcon fontSize='large'/>
+            <PlayArrowIcon fontSize='large'/>
+            <SkipNextRoundedIcon fontSize='large'/>
+        </div>
+        <progress className='Progress' value="100" max="100"></progress>
         
     </div>
   )
