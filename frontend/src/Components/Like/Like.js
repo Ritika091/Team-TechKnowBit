12fe87dca0cbc5376caf9f96e7ade7be6b105f2c
Swarga-codes <sd3526@srmist.edu.in>
2023-04-23 23:03:37 +0530

    added like page

--- frontend/src/Components/Like/Like.js
+++ frontend/src/Components/Like/Like.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Like.css'
 import Sidebar from '../Sidebar/Sidebar'
 import UpperNav from '../UpperNav/UpperNav'
 import {Link} from 'react-router-dom'
 import albumpic from '../../assets/podcast.jpg'
 export default function Like() {
+  const[likePods,setLikePods]=useState([])
+  useEffect(()=>{
+   fetch('http://localhost:5000/likedpodcasts',{
+    headers:{
+      'Content-Type':'application/json',
+      'Authorization':'Bearer '+localStorage.getItem('jwt')
+  },
+   })
+   .then(res=>res.json())
+   .then(result=>{
+    console.log(result)
+    setLikePods(result)
+   })
+   .catch(err=>console.log(err)) 
+  },[])
   return (
     <div className='Like'>
         <Sidebar/>
         <div className="LikeRightpart">
        <UpperNav/>
-       <div className="LikeContent">       
+       <div className="LikeContent">   
+       {likePods.map(like=>(
         <div className="Likepodcast_audio">
-            <img src={albumpic}></img>
-        <p className='Likepodtitle'>Title</p>
-        <p className='Likepodspeaker'>Speaker</p>
-        <p className='Likepodtype'>Type</p>
+            <img src={albumpic}/>
+        <p className='Likepodtitle'>{like.title}</p>
+        <p className='Likepodspeaker'>{like.speaker}</p>
+        <p className='Likepodtype'>{like.type}</p>
         
-        <button className='Likepodbtn'>Show details</button>
         
+  <Link to={`/podcasts/${like._id}`}><button className='Likepodbtn'>Show details</button></Link>
         <br />
         </div>
+        ))    }
        </div>
        </div>
     </div>
