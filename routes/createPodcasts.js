8588a7a4f56a0e4a24ec6a6f9b36064877269882
Faisal Nazir <76447944+faisalnazir7@users.noreply.github.com>
2023-04-23 18:04:18 +0530

    like and unlike route

--- routes/createPodcasts.js
+++ routes/createPodcasts.js
@@ -51,6 +51,35 @@ if(!audioFile && videoFile){
     .catch(err=>console.log(err))
 }
 })
+
+router.put('/like',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/unlike',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
 router.get('/podcasts/:podcastId',VerifyLogin,(req,res)=>{
    PODCASTS.findById({_id:req.params.podcastId})
    .then(result=>{
