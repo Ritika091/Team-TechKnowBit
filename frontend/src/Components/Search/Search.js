1676cb570fe6e32653fefa94fbb52e121d1056d7
Swarga-codes <sd3526@srmist.edu.in>
2023-04-23 14:26:18 +0530

    added search functionality

--- frontend/src/Components/Search/Search.js
+++ frontend/src/Components/Search/Search.js
@@ -1,21 +1,75 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import Avatar from '@mui/material/Avatar';
 import Sidebar from '../Sidebar/Sidebar';
 import './Search.css'
+import { Link } from 'react-router-dom';
 
 export default function Search() {
+  const[searchInput,setSearchInput]=useState("");
+  const[podcasts,setPodcasts]=useState([])
+  const[result,setResult]=useState("");
+  const[errMessage,setErrMessage]=useState("");
+    const fetchPodcasts=()=>{
+        fetch('http://localhost:5000/podcasts',{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':'Bearer '+localStorage.getItem('jwt')
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{console.log(data);
+         setPodcasts(data)   
+        })
+        .catch(err=>console.log(err))
+    }
+    const searchPodcasts=()=>{
+      let res=podcasts.find(attr=>{
+        return attr.title===searchInput||attr.speaker===searchInput||attr.category===searchInput 
+      })
+      if(!res){
+        console.log('Not found')
+        setResult('')
+        setErrMessage('Not Found')
+      }else{
+
+      setResult(res)
+      console.log(result)
+      }
+      setSearchInput('');
+    }
+    useEffect(()=>{
+        fetchPodcasts();
+    },[result]);
   return (
     <div className='Search'>
         <Sidebar/>
         <div className="nav">
          <div className="SearchSec">
-       <SearchIcon className='searchIcon' />
-       <input type="text" placeholder="What do you want to listen?" name="search"></input>
+       <SearchIcon className='searchIcon' onClick={()=>{searchPodcasts();
+     
+       }}/>
+       <input type="text" placeholder="What do you want to listen?" value={searchInput} name="search" onChange={(e)=>setSearchInput(e.target.value)}></input>
        </div>
        <div className="profile">
        <Avatar alt="Remy Sharp" src=""  className='Ava'/>
        <h3>{JSON.parse(localStorage.getItem("users")).userName}</h3>
+       </div>
+       <div className="search_results">
+       <h1>Search Results</h1>
+       
+      {result?
+          <div className="result_card">
+          <p>{result.title}</p>
+          <p>{result.speaker}</p>
+          <p>{result.category}</p>
+          <Link to={result.audioFile?result.audioFile:result.videoFile}>Listen!</Link>
+          </div>
+          :
+          <p>{errMessage}</p>
+        
+      }
        </div>
     </div>
     </div>
